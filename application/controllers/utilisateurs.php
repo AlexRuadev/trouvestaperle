2e23c2bfced6324861c637147941aeffd54d5ee5
camillelupo <camille.lupo@hotmail.fr>
2019-01-31 14:49:47 +0100

    commit tentative merge

--- application/controllers/utilisateurs.php
+++ application/controllers/utilisateurs.php
@@ -1,29 +1,109 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 23/01/2019
+ * Time: 13:35
+ */
 
-class utilisateurs extends CI_Controller {
-
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     *	- or -
-     * 		http://example.com/index.php/welcome/index
-     *	- or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
-     */
-    public function inscription()
+class Utilisateurs extends CI_Controller
+{
+    public function __construct()
     {
-        $this->load->helper('url');
-        $this->load->view('template/header.php');
-        $this->load->view('formulaire_cv.php');
-        $this->load->view('template/footer.php');
-
+        parent::__construct();
+        $this->load->model("UtilisateursModel");
     }
+
+    //Controller du formulaires d'inscription
+    public function create()
+    {
+
+        $this->db->set('utilisateurs_created_at ', 'NOW()', false);
+
+        $this->form_validation->set_rules('nom', 'nom', 'required');
+        $this->form_validation->set_rules('prenom', 'prenom', 'required');
+        $this->form_validation->set_rules('mail', 'mail', 'required');
+        $this->form_validation->set_rules('motdepasse', 'motdepasse', 'required');
+
+        function generateRandomString($length = 10)
+        {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
+
+        if ($this->form_validation->run() === FALSE) {
+
+            $this->load->view('template/header.php');
+            $this->load->view('formulaire_cv.php');
+            $this->load->view('template/footer.php');
+
+        } else {
+            $hash = password_hash($this->input->post("motdepasse"), PASSWORD_DEFAULT);
+            $token = generateRandomString();
+
+            $this->UtilisateursModel->inscription($hash, $token);
+            $this->load->view('success');
+        }
+    }
+
+    //Controller du formulaire de connexion
+    public function login()
+    {
+
+        $data = new stdClass();
+
+        $mail = $this->input->post('mail');
+        $motdepasse = $this->input->post('motdepasse');
+
+        $user = $this->UtilisateursModel->test_mail($mail);
+
+
+        $this->form_validation->set_rules('mail', 'mail', 'required');
+        $this->form_validation->set_rules('motdepasse', 'motdepasse',  'required');
+
+        if ($this->form_validation->run() === FALSE) {
+
+            $this->load->view('login');
+
+        } else {
+
+            if (isset($user[0]['utilisateurs_mail'])) {
+
+                if (password_verify($motdepasse, $user[0]['utilisateurs_motdepasse'])) {
+
+                    $newdata = array(
+                        'utilisateurs_id' => $user[0]['utilisateurs_id'],
+                        'utilisateurs_nom' => $user[0]['utilisateurs_nom'],
+                        'utilisateurs_mail' => $user[0]['utilisateurs_mail'],
+                        'logged_in' => true
+                    );
+                    $this->session->set_userdata($newdata);
+                    $this->load->view('success');
+
+                } else {
+
+                    $data->error = 'Email ou mot de passe invalide.';
+                    $this->load->view('login', $data);
+                }
+            } else{
+                $data->error = 'Email ou mot de passe invalide.';
+                $this->load->view('login', $data);
+            }
+        }
+    }
+
+    //Deconnexion
+    public function deco(){
+
+        $this->session->sess_destroy();
+        $this->load->view('login');
+    }
+
+
 }
+
