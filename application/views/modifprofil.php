b5a1c9399fe73e653dab07a29740534c67618916
camillelupo <camille.lupo@hotmail.fr>
2019-03-07 11:35:49 +0100

    css profil

--- application/views/modifprofil.php
+++ application/views/modifprofil.php
@@ -2,55 +2,89 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<div class="container">
-	<form class="modifprofil" action="<?php echo base_url(); ?>Utilisateurs/modifProfil/<?php echo $_SESSION['utilisateurs_id'] ?>"  method="post">
 
+<div class="contain">
+    <form class="modifprofil" action="<?php echo base_url(); ?>Utilisateurs/modifProfil/<?php echo $_SESSION['utilisateurs_id'] ?>"  method="post">
+    <div class="modifprofil">
 
-        <div class="form-group row">
-        <label for="staticNom" class="col-sm-2 col-form-label">Nom:</label>
-		<input type="text" name="nom" id="nom" value="<?php echo $donnée['utilisateurs_nom'] ?>">
-		<?php echo form_error('nom') ?>
+        <div class="table-users">
+
+            <div class="header">Profil</div>
+
+            <table cellspacing="0">
+
+                <tr>
+                    <td>Nom :</td>
+                    <td>
+                        <input type="text" name="nom" class="profilInput" id="nom" value="<?php echo $donnée['utilisateurs_nom'] ?>">
+                        <?php echo form_error('nom') ?>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                        </td>
+                </tr>
+
+                <tr>
+                    <td>Prénom :</td>
+                    <td>
+                        <input type="text" name="prenom" class="profilInput" id="nom" value="<?php echo $donnée['utilisateurs_prenom'] ?>">
+                        <?php echo form_error('prenom') ?>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Email :</td>
+                    <td>
+                        <input type="text" name="mail" class="profilInput" id="nom" value="<?php echo $donnée['utilisateurs_mail'] ?>">
+                        <?php echo form_error('mail') ?>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Téléphone :</td>
+                    <td>
+                        <input type="text" name="telephone" class="profilInput" id="nom" value="<?php echo $donnée['utilisateurs_num'] ?>">
+                        <?php echo form_error('telephone') ?>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Date de Naissance:</td>
+                    <td>
+                        <input type="date" name="naissance" class="profilInput" id="nom" value="<?php echo $donnée['utilisateurs_naissance'] ?>">
+                        <?php echo form_error('naissance') ?>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Code postal :</td>
+                    <td>
+                        <input type="text" name="codepostal" class="profilInput" id="nom" value="<?php echo $donnée['utilisateurs_codepostal'] ?>">
+                        <?php echo form_error('codepostal') ?>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Permis :</td>
+                    <td>
+                        <input type="text" name="permis" class="profilInput" id="nom" value="<?php echo $donnée['utilisateurs_permis'] ?>">
+                        <?php echo form_error('permis') ?>
+                        <span class="highlight"></span>
+                        <span class="bar"></span>
+                    </td>
+                </tr>
+            </table>
+            </form>
+            <div class="modifbutton"><input type="submit" class="buttonmodifier " name="submitedit" value="Enregistrer"></div>
+
         </div>
 
-        <div class="form-group row">
-        <label for="staticPrénom" class="col-sm-2 col-form-label">Prénom:</label>
-		<input type="text" name="prenom" id="prenom" value="<?php echo $donnée['utilisateurs_prenom'] ?>">
-		<?php echo form_error('prenom') ?>
-        </div>
-
-        <div class="form-group row">
-            <label for="staticEmail" class="col-sm-2 col-form-label">Email:</label>
-		<input type="email" name="mail" id="mail" value="<?php echo $donnée['utilisateurs_mail'] ?>">
-		<?php echo form_error('mail') ?>
-        </div>
-
-        <div class="form-group row">
-            <label for="staticPhone" class="col-sm-2 col-form-label">Téléphone:</label>
-		<input type="text" name="telephone" id="téléphone" value="<?php echo $donnée['utilisateurs_num'] ?>">
-		<?php echo form_error('telephone') ?>
-        </div>
-
-        <div class="form-group row">
-            <label for="staticDateDeNaissance" class="col-sm-2 col-form-label">Date de Naissance:</label>
-		<input type="date" name="naissance" id="naissance" value="<?php echo $donnée['utilisateurs_naissance'] ?>">
-		<?php echo form_error('naissance') ?>
-        </div>
-
-        <div class="form-group row">
-            <label for="staticCodePostal" class="col-sm-2 col-form-label">Code Postal:</label>
-		<input type="text" name="codepostal" id="codepostal" value="<?php echo $donnée['utilisateurs_codepostal'] ?>">
-		<?php echo form_error('codepostal') ?>
-        </div>
-
-        <div class="form-group row">
-            <label for="staticPermis" class="col-sm-2 col-form-label">Permis obtenu:</label>
-		<input type="text" name="permis" id="permis" value="<?php echo $donnée['utilisateurs_permis'] ?>">
-		<?php echo form_error('permis') ?>
-        </div>
-
-
-		<br><input type="submit" class="buttonmodifier" name="submitedit" value="Enregistrer">
-	</form>
-</div>
+    </div>
 
 
--- application/views/profil.php
+++ application/views/profil.php
@@ -5,56 +5,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="contain">
         <div class="modifprofil">
 
-        <div class="form-group row">
-        <label for="staticNom" class="col-sm-2 col-form-label">Nom:</label>
-            <div class="col-sm-10">
-                <input type="text" readonly class="form-control-plaintext" id="staticNom" value="<?php echo $donnée['utilisateurs_nom'] ?>">
-            </div>
-        </div>
+            <div class="table-users">
 
-        <div class="form-group row">
-        <label for="staticPrénom" class="col-sm-2 col-form-label">Prénom:</label>
-            <div class="col-sm-10">
-            <input type="text" readonly class="form-control-plaintext" id="staticPrénom" value="<?php echo $donnée['utilisateurs_prenom'] ?>">
-            </div>
-		</div>
+                <div class="header">Profil</div>
 
-        <div class="form-group row">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Email:</label>
-            <div class="col-sm-10">
-            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="<?php echo $donnée['utilisateurs_mail'] ?>">
-            </div>
-        </div>
+                <table cellspacing="0">
 
-        <div class="form-group row">
-        <label for="staticPhone" class="col-sm-2 col-form-label">Téléphone:</label>
-            <div class="col-sm-10">
-            <input type="text" readonly class="form-control-plaintext" id="staticPhone" value="<?php echo $donnée['utilisateurs_num'] ?>">
-            </div>
-        </div>
+                    <tr>
+                        <td>Nom :</td>
+                        <td><?php echo $donnée['utilisateurs_nom'] ?></td>
+                    </tr>
 
-        <div class="form-group row">
-        <label for="staticDateDeNaissance" class="col-sm-2 col-form-label">Date de Naissance:</label>
-            <div class="col-sm-10">
-            <input type="text" readonly class="form-control-plaintext" id="staticDateDeNaissance" value="<?php echo strftime('%d/%m/%Y',strtotime($donnée['utilisateurs_naissance'])) ?>">
-            </div>
-        </div>
+                    <tr>
+                        <td>Prénom :</td>
+                        <td><?php echo $donnée['utilisateurs_prenom'] ?></td>
 
+                    </tr>
 
-        <div class="form-group row">
-        <label for="staticCodePostal" class="col-sm-2 col-form-label">Code Postal:</label>
-            <div class="col-sm-10">
-            <input type="text" readonly class="form-control-plaintext" id="staticCodePostal" value="<?php echo$donnée['utilisateurs_codepostal'] ?>">
-            </div>
-        </div>
+                    <tr>
+                        <td>Email :</td>
+                        <td><?php echo $donnée['utilisateurs_mail'] ?></td>
+                    </tr>
 
-            <div class="form-group row">
-                <label for="staticPermis" class="col-sm-2 col-form-label">Permis:</label>
-                <div class="col-sm-10">
-                    <input type="text" readonly class="form-control-plaintext" id="staticPermis" value="<?php echo $donnée['utilisateurs_permis'] ?>">
-                </div>
+                    <tr>
+                        <td>Téléphone :</td>
+                        <td><?php echo $donnée['utilisateurs_num'] ?></td>
+
+                    </tr>
+
+                    <tr>
+                        <td>Date de Naissance:</td>
+                        <td><?php echo strftime('%d/%m/%Y',strtotime($donnée['utilisateurs_naissance']))?></td>
+                    </tr>
+                    <tr>
+                        <td>Code postal :</td>
+                        <td><?php echo $donnée['utilisateurs_codepostal'] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Permis :</td>
+                        <td><?php echo $donnée['utilisateurs_permis'];?></td>
+                    </tr>
+                </table>
+                <div class="modifbutton"><a class="block-center" href="<?php echo base_url(); ?>Utilisateurs/modifProfil/<?php echo $_SESSION['utilisateurs_id'] ?>"><button              class="buttonmodifier ">Modifier</button></a></div>
+
             </div>
 
-
-	<a href="<?php echo base_url(); ?>Utilisateurs/modifProfil/<?php echo $_SESSION['utilisateurs_id'] ?>"><button class="buttonmodifier">Modifier</button></a>
         </div>
